7c3b2cba2ba071fc666ccc14eb7e744a7184890b
Ramon Alejandro <78730-22250534@SERVER2.DOMAIN>
2024-05-11 13:19:38 +0800

    second commit

--- src/productDetails.jsx
+++ src/productDetails.jsx
@@ -28,7 +28,7 @@ function ProductDetails() {
     const stripe = await stripePromise;
 
     // Send a request to the backend to create a checkout session
-    const response = await fetch('http://localhost:4000/create-checkout-session', {
+    const response = await fetch('http://35.240.139.36/create-checkout-session', {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json',
